9b1b9f617972da5b24599be3e7809870391ad6f7
nejha <nejhad.maryam@gmail.com>
2022-07-30 12:53:25 +1000

    week 1

---
+++ intro.py
@@ -0,0 +1,27 @@
+# This program prints Hello, world!
+
+# This program adds two numbers
+
+# animals list
+animals = ['cat', 'dog', 'rabbit']
+
+# Add 'guinea pig' to the list
+animals.append('guinea pig')
+
+
+print('Updated animals list: ', animals)
+
+
+
+# create a list of prime numbers
+prime_numbers = [2, 3, 5, 7]
+
+# remove the element at index 2
+removed_element = prime_numbers.pop(2)
+
+print('Removed Element:', removed_element)
+print('Updated List:', prime_numbers)
+
+# Output:
+# Removed Element: 5
+# Updated List: [2, 3, 7]
